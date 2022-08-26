645aa65e32664974630bba531b724bcbd32aab7d
nevengjoreski <nenoseksovski@hotmail.com>
2022-08-26 22:01:57 +0200

    cas3

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { LifecycleClassComponent } from "./components/LifecycleClassComponent"
+import { LifecycleFuncComponent } from "./components/LifecycleFuncComponent"
+import { LogIn } from "./components/LogIn"
+import {useState} from 'react'
+
+const App = () => {
+
+  const [show , setShow] = useState(true)
+
+  
+  return (<div>
+    
+    <button
+      style={{padding:"10px", width:"100%"}}
+        onClick={() => setShow((s) => !s)}
+      >Show/Hide</button>
+
+      {show && <LifecycleClassComponent />}
+
+      {show && <LifecycleFuncComponent />}
+
+      <LogIn />
+
+  </div>);
+}
+
+export default App
